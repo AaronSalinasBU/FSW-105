a47017446e0737a0955d855fb859a8fce426e389
AaronSalinasBU <58755799+AaronSalinasBU@users.noreply.github.com>
2020-02-15 18:21:58 -0600

    Week1-Step5

---
+++ Week1/grocery_store.js
@@ -0,0 +1,12 @@
+var shopper = {
+    firstName: "Michael",
+    lastName: "Salinas",
+    age: 30,
+    groceryCart: ["cookies","meat","dairy","bread","napkins"],
+    fullName : function() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+var buyCandy = false;
+
+console. log()
