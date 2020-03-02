9c2ce35a3254b71e84d1ebcce29fadf44cbadb32
AaronSalinasBU <58755799+AaronSalinasBU@users.noreply.github.com>
2020-03-01 20:27:49 -0600

    mastery submission

--- Week2/Olympics.js
+++ Week2/Olympics.js
@@ -8,10 +8,6 @@ if (person.age >= 18 || person.name === "B") {
     console.log("bobby can not go to the movies")
 };
 //silver medal
-var number = {
-    string : "1",
-    num: 1 
-}
 if (1 === "1") {
 console.log("strict")
 }else if(1 =="1" ) {
@@ -22,7 +18,7 @@ console.log("strict")
 //Gold Medal
 var isString = ("dog" === "dog") ? "is a string" : "is not a string";
 console.log(isString);
-var isBoolean = ("true" === "true") ? "is a boolean": "is not a boolean";
+var isBoolean = ( typeof true === "boolean") ? "is a boolean": "is not a boolean";
 console.log(isBoolean);
 var beenDefined = ("var" === "var") ? "var has been defined" : "var has not been defined";
 console.log(beenDefined);
